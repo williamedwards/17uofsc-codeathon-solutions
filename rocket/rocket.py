97c6eb7731c1da4b392b0ea8841f8d0f150292a9
William Edwards <williamedwards314@gmail.com>
2017-10-13 23:20:29 -0400

    First pass at rocket, untested.

---
+++ rocket/rocket.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+# Representation
+# Tuple (x, y, z, type)
+#   x, y: coords on grid, zero index from top left
+#   z: level 0 index
+#   type in {unwalkable, walkable, entry point, staircase, warp point, pikachu}
+
+import heapq
+
+# Define Constants
+NOWALK = 0
+WALK = 1
+ENTER = 2
+STAIR = 3
+WARP = 4
+PIKACHU = 5
+
+INF = 1000
+
+num_levels = int(input())
+level_dims = [int(val) for val in input().split(" ")]
+
+# We will represent tuple (tentative distance, type)
+# We can just compute one level at a time
+for level_dim in level_dims:
+    input() # Read blank line
+    grid = []
+    entry_coords = None
+    dest_coords = None # Stairs or pikachu
+    warp_cords = []
+
+    total_path_length = 0
+    for i in range(level_dims):
+        row = []
+        for j, char in enumerate(input()):
+            if char == "#":
+                row.append((False, NOWALK, INF))
+            if char == " ":
+                row.append((False, WALK, INF))
+            if char == "s":
+                row.append((False, STAIR, INF))
+                dest_coords = (i, j)
+            if char == "s":
+                row.append((False, PIKACHU, INF))
+                dest_coords = (i, j)
+            if char == "e":
+                row.append((False, ENTER, INF))
+                entry_coords = (i, j)
+            if char == "w":
+                row.append((False, WARP, INF))
+                warp_coords.append[(i, j)]
+        unvisited = [(0, entry_coords[0], entry_coords[1])]
+        while True:
+            current = heapq.heappop(unvisited)
+            grid_square = grid[current[1]][current[2]]
+            if(grid_square[0]):
+                # This has already been visited
+                continue
+            if(grid_square[1] == STAIR or grid_square[1] == PIKACHU):
+                total_path_length += current[0]
+                break
+            # Check adjacent squares
+            for xoff, yoff in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                try:
+                    other_grid_square = \
+                        grid[current[1] + xoff][current[2] + yoff]
+                    if other_gird_square[1] == NOWALK:
+                        continue
+                    if current[0] + 1 < other_grid_square[0]:
+                        other_grid_square[0] = current[0] + 1
+                        heapq.heappush(
+                            unvisited, (other_grid_square[0],
+                                        current[1] + xoff,
+                                        current[2] + yoff))
+            # Check for warp points
+            if grid_square[1] == WARP:
+                warp_point = warp_coords[0]
+                if (current[1], current[2]) == warp_point:
+                    warp_point = warp_coords[1]
+                other_grid_square = grid[warp_point[0]][warp_point[1]]
+                if current[0] + 1 < other_grid_square[0]:
+                    other_grid_square[0] = current[0] + 1
+                    heapq.heappush(
+                        unvisited, (other_grid_square[0],
+                                    warp_point[0],
+                                    warp_point[1]))
+print(total_path_length)
+
