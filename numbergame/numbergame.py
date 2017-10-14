cb09bcb0aafb1e81fedcde50f75ec35d585ba5cd
William Edwards <williamedwards314@gmail.com>
2017-10-13 20:51:12 -0400

    Finished caching version.

--- numbergame/numbergame.py
+++ numbergame/numbergame.py
@@ -8,15 +8,25 @@ def print_guy(turn):
     else:
         print("FENNER")
 
+def update_cached(cached, values, turn_mask):
+    for value in values:
+        cached[value] = turn_mask
+
 T = int(input())
 Ns = []
 for i in range(T):
     Ns.append(int(input()))
 
-# Super Naive Version
+cached = {}
+
 for N in Ns:
     turn = BUELL
+    values = []
     while True:
+        if N in cached:
+            print_guy(cached[N] ^ turn)
+            break
+        values.append(N)
         # Compute <= power of 2
         temp = N
         p = 0
@@ -26,9 +36,11 @@ for N in Ns:
         next_lowest_pow = 1 << p
         if N == next_lowest_pow:
             if p % 2 == 1:
+                update_cached(cached, values, 0)
                 print_guy(turn)
             else:
                 turn ^= 1
+                update_cached(cached, values, 1)
                 print_guy(turn)
             break
         else:
