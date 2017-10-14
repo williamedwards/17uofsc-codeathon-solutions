77285f0659363142786e863ccf17991cae94c6af
William Edwards <williamedwards314@gmail.com>
2017-10-13 21:14:43 -0400

    YESSS

--- numbergame/numbergame.py
+++ numbergame/numbergame.py
@@ -1,49 +1,24 @@
 #!/usr/bin/python3
 
-BUELL = 0
-FENNER = 1
-def print_guy(turn):
-    if turn == BUELL:
-        print("BUELL")
-    else:
-        print("FENNER")
-
-def update_cached(cached, values, turn_mask):
-    for value in reversed(values):
-        cached[value] = turn_mask
-        turn_mask ^= 1
-
 T = int(input())
 Ns = []
 for i in range(T):
     Ns.append(int(input()))
 
-cached = {}
 
 for N in Ns:
-    turn = BUELL
-    values = []
-    while True:
-        if N in cached:
-            print_guy(cached[N] ^ turn)
-            break
-        values.append(N)
-        # Compute <= power of 2
-        temp = N
-        p = 0
-        while temp > 1:
-            temp = temp >> 1
-            p += 1
-        next_lowest_pow = 1 << p
-        if N == next_lowest_pow:
-            if p % 2 == 1:
-                update_cached(cached, values, 0)
-                print_guy(turn)
-            else:
-                turn ^= 1
-                update_cached(cached, values, 1)
-                print_guy(turn)
-            break
-        else:
-            N -= next_lowest_pow
-        turn ^= 1
+    # Count ones and locate rightmost
+    num_ones = 0
+    pos_rightmost_one = -1
+    i = 0
+    while N > 0:
+        if N & 1 == 1:
+            num_ones += 1
+            if pos_rightmost_one == -1:
+                pos_rightmost_one = i
+        i += 1
+        N >>= 1
+    if (pos_rightmost_one + num_ones) % 2 == 0:
+        print("BUELL")
+    else:
+        print("FENNER")
